0142e1ed995ecb7440b1fc400aa44c48d4640d0b
npaj <nicolas.pajusco@gmail.com>
2022-08-09 15:06:35 +0200

    up readme

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # EAVT
 Eco-Acoustic Visualization Tools
 
+## Audio processing
+**First download the ResNet22 pretrained model using instructions [here](https://github.com/qiuqiangkong/audioset_tagging_cnn#audio-tagging-using-pretrained-models)**
+
+process.py [-h] [--data_path DATA_PATH] [--save_path SAVE_PATH]
+
+Script to process sound files recorded by Audiomoth
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --data_path DATA_PATH
+                        Path to wav files
+  --save_path SAVE_PATH
+                        Path to save meta data
+
+## Dash app
+
+dash_app.py [-h] [--save_path SAVE_PATH]
+
+Script to display sound files recorded by Audiomoth
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --save_path SAVE_PATH
+                        Path to save meta data
+
+                        
 
 Code for Audioset Tagging CNN from [Qiu Qiang Kong](https://github.com/qiuqiangkong/audioset_tagging_cnn)
--- process.py
+++ process.py
@@ -13,6 +13,10 @@ from audioset_tagging_cnn.inference import audio_tagging
 
 from indicies import name_indicies
 
+# In process
+PROCESS_TAG = True
+PROCESS_Indices = True
+
 
 checkpoint_path = 'ResNet22_mAP=0.430.pth'
 
@@ -23,33 +27,41 @@ args = parser.parse_args()
 
 csvfile = os.path.join(args.save_path, 'indices.csv')
 audio_savepath = os.path.join(args.save_path, 'audio')
+if not os.path.exists(audio_savepath):
+    os.makedirs(audio_savepath)
 
 # get meta data file
 df_files = metadata.metadata_generator(args.data_path)
 
 # get data loader
 dl = dataloader.get_dataloader_site(args.data_path, df_files, Fmin = 1, Fmax = 10**5, savepath = audio_savepath, batch_size = 12)
-
-df_site = {'datetime':[], 'name':[], 'start':[], 'clipwise_output' : [], 'embedding' :[], 'sorted_indexes':[]}
-for ii in name_indicies: df_site[ii] = []
+df_site = {'datetime':[], 'name':[], 'start':[]}
+if PROCESS_TAG:
+    df_site['clipwise_output'] =  []
+    df_site['embedding'] =[], 
+    df_site['sorted_indexes'] = []
+if PROCESS_Indices:
+    for ii in name_indicies: df_site[ii] = []
 
 for batch_idx, (inputs, info) in enumerate(tqdm(dl)):
-
-    with torch.no_grad():
-        clipwise_output, labels, sorted_indexes, embedding = audio_tagging(inputs, checkpoint_path , usecuda=False)
+    if PROCESS_TAG:
+        with torch.no_grad():
+            clipwise_output, labels, sorted_indexes, embedding = audio_tagging(inputs, checkpoint_path , usecuda=False)
 
     for idx, date_ in enumerate(info['date']):
         df_site['datetime'].append(str(date_)) 
         df_site['name'].append(str(info['name'][idx]))
         df_site['start'].append(float(info['start'][idx]))
-        df_site['clipwise_output'].append(clipwise_output[idx])
-        df_site['sorted_indexes'].append(sorted_indexes[idx])
-        df_site['embedding'].append(embedding[idx])
-        for key in info['ecoac'].keys():
-            df_site[key].append(float(info['ecoac'][key].numpy()[idx])) 
-    
-
-Df_tagging = tagging_validate(df_site)
+        if PROCESS_TAG:
+            df_site['clipwise_output'].append(clipwise_output[idx])
+            df_site['sorted_indexes'].append(sorted_indexes[idx])
+            df_site['embedding'].append(embedding[idx])
+        if PROCESS_Indices:
+            for key in info['ecoac'].keys():
+                df_site[key].append(float(info['ecoac'][key].numpy()[idx])) 
+        
+if PROCESS_TAG:
+    Df_tagging = tagging_validate(df_site)
 
 
 ## Dataframe with only ecoacoustic indices and important metadata 
